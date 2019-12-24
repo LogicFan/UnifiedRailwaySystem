7e2b9dc600ab76dd02281907e84098791ab22739
LogicFan <38597904+LogicFan@users.noreply.github.com>
2019-12-24 18:05:09 +0800

    create project

---
+++ UnifiedRailwaySystem/URSMod.cs
@@ -0,0 +1,18 @@
+﻿using ICities;
+using UnityEngine;
+
+namespace UnifiedRailwaySystem
+{
+    public class URSMod : IUserMod
+    {
+        public string Name
+        {
+            get { return "Unified Railway System"; }
+        }
+
+        public string Description
+        {
+            get { return "Let Train and Metro use the same railway."; }
+        }
+    }
+}
