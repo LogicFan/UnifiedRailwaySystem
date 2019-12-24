9570f24abf664f23c9809aed73f352139ed43924
LogicFan <38597904+LogicFan@users.noreply.github.com>
2019-12-24 22:11:35 +0800

    add first patch

--- UnifiedRailwaySystem/URSMod.cs
+++ UnifiedRailwaySystem/URSMod.cs
@@ -1,10 +1,15 @@
 ﻿using ICities;
 using UnityEngine;
+using Harmony;
+using System.Reflection;
 
 namespace UnifiedRailwaySystem
 {
-    public class URSMod : IUserMod
+    public class URSMod : IUserMod, ILoadingExtension
     {
+        HarmonyInstance harmony;
+
+        #region IUserMod
         public string Name
         {
             get { return "Unified Railway System"; }
@@ -14,5 +19,25 @@ namespace UnifiedRailwaySystem
         {
             get { return "Let Train and Metro use the same railway."; }
         }
+        #endregion
+
+        #region ILoadingExtension
+        public void OnCreated(ILoading loading) { }
+
+        public void OnLevelLoaded(LoadMode mode)
+        {
+            Debug.Log("URSMod.OnLevelLoaded");
+            harmony = HarmonyInstance.Create("com.logic.urs");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        public void OnLevelUnloading()
+        {
+            Debug.Log("URSMod.OnLevelUnloading");
+            harmony.UnpatchAll("com.logic.urs");
+        }
+
+        public void OnReleased() { }
+        #endregion
     }
 }
---
+++ UnifiedRailwaySystem/URSNetManager.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Harmony;
+using UnityEngine;
+
+namespace UnifiedRailwaySystem
+{
+    namespace URSNetManager
+    {
+        [HarmonyPatch(typeof(NetManager))]
+        [HarmonyPatch("CreateSegment")]
+        public class URSCreateSegament
+        {
+            public static bool Prefix(ref NetInfo info)
+            {
+                Debug.Log("NetManager.CreateSegament, info: " + info);
+                return true;
+            }
+        }
+    }
+}
