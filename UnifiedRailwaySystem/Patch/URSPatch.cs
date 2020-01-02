3e6691c31fc07e9de1e874e85aeaf54d2d6f64b4
LogicFan <38597904+LogicFan@users.noreply.github.com>
2020-01-03 02:00:07 +0800

    add documentation

--- UnifiedRailwaySystem/Patch/URSPatch.cs
+++ UnifiedRailwaySystem/Patch/URSPatch.cs
@@ -4,20 +4,31 @@ using UnityEngine;
 
 namespace UnifiedRailwaySystem
 {
+    /// <summary>
+    /// A class to control all Harmony patches.
+    /// </summary>
     public static class URSPatch
     {
         private static HarmonyInstance harmony;
 
+        /// <summary>
+        /// Patch all harmony patches.
+        /// </summary>
         public static void Patch()
         {
             Debug.Log("URSPatch.Patch");
+
             harmony = HarmonyInstance.Create("com.logic.urs");
             harmony.PatchAll(Assembly.GetExecutingAssembly());
         }
 
+        /// <summary>
+        /// Unpatch all harmony patches.
+        /// </summary>
         public static void Unpatch()
         {
             Debug.Log("URSPatch.Unpatch");
+            
             harmony.UnpatchAll("com.logic.urs");
         }
     }
--- UnifiedRailwaySystem/URSUtil.cs
+++ UnifiedRailwaySystem/URSUtil.cs
@@ -1,12 +1,16 @@
 ﻿using ICities;
 using UnityEngine;
 
-// currently not used
-
 namespace UnifiedRailwaySystem
 {
+    /// <summary>
+    /// A utility class for the mod.
+    /// </summary>
     public static class Util
     {
+        /// <summary>
+        /// A class that contains information will be later used in the mod.
+        /// </summary>
         public static class Cache
         {
             #region TrackItemClass
@@ -25,6 +29,9 @@ namespace UnifiedRailwaySystem
             public static NetInfo.ConnectGroup tramConnectGroup => _tramConnectGroup;
             #endregion
 
+            /// <summary>
+            /// Initialize the class.
+            /// </summary>
             public static void Initialize()
             {
                 _trainTrackItemClass = PrefabCollection<NetInfo>.FindLoaded("Train Track").m_class;
@@ -32,34 +39,41 @@ namespace UnifiedRailwaySystem
                 _tramTrackItemClass = PrefabCollection<NetInfo>.FindLoaded("Basic Road").m_class;
 
                 _trainConnectGroup = NetInfo.ConnectGroup.DoubleTrain
-                | NetInfo.ConnectGroup.SingleTrain
-                | NetInfo.ConnectGroup.TrainStation;
+                    | NetInfo.ConnectGroup.SingleTrain
+                    | NetInfo.ConnectGroup.TrainStation;
                 _tramConnectGroup = NetInfo.ConnectGroup.CenterTram
-                | NetInfo.ConnectGroup.NarrowTram
-                | NetInfo.ConnectGroup.SingleTram
-                | NetInfo.ConnectGroup.WideTram;
+                    | NetInfo.ConnectGroup.NarrowTram
+                    | NetInfo.ConnectGroup.SingleTram
+                    | NetInfo.ConnectGroup.WideTram;
             }
         }
 
+        /// <summary>
+        /// Initialize the class.
+        /// </summary>
         public static void Initialize()
         {
             Debug.Log("Util.Initialize");
             Cache.Initialize();
         }
 
+        /// <summary>
+        /// A clone method for <c>PrefabInfo</c>
+        /// </summary>
+        /// <param name="originalPrefab"> original <c>PrefabInfo</c> will be cloned. </param>
+        /// <typeparam name="T"> the type of <c>originalPrefab</c>. </typeparam>
+        /// <returns> a clone of <c>originalPrefab</c>. </returns>
         public static T Clone<T>(T originalPrefab) where T : PrefabInfo
         {
             var instance = Object.Instantiate(originalPrefab.gameObject);
-
-            // magical code comes from MOM
             instance.transform.parent = originalPrefab.gameObject.transform;
-            // instance.transform.SetParent(transform);
-            // instance.transform.localPosition = new Vector3(-7500, -7500, -7500);
 
             var newPrefab = instance.GetComponent<T>();
             instance.SetActive(false);
+
             // This value will set to be true at PrefabCollection<T>.InitializePrefabImpl
             newPrefab.m_prefabInitialized = false;
+            
             return newPrefab;
         }
     }
